74e451f58a1ef9186ead19fb9abb5c78317408fa
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-05-27 11:17:11 +0200

    auto: HeroSection.tsx

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -65,7 +65,7 @@ export function HeroSection() {
 					</a>
 					{/* Headline */}
 					<h1 className="text-4xl font-bold text-white md:text-[3.4rem] leading-tighter">
-						Reliable TypeScript for the AI era
+						Production-ready TypeScript for the AI era
 					</h1>
 
 					{/* Subheadline */}
