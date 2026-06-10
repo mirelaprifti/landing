c97a7a2f32da5c33dafd0f2f7c89afa881460bce
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-06-10 16:47:50 +0200

    auto: freckle-icon.png,effect-jobs.ts

--- src/data/effect-jobs.ts
+++ src/data/effect-jobs.ts
@@ -40,7 +40,7 @@ export const JOBS: Job[] = [
 		role: "Full-stack Engineers",
 		url: "https://discord.gg/effect-ts",
 		note: "Posted on Discord #job-board",
-		logo: "/assets/effect-jobs-logos/freckle-logo.svg",
+		logo: "/assets/effect-jobs-logos/freckle-icon.png",
 	},
 	{
 		company: "Gale",
