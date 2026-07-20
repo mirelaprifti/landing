0053c9ff1af84ae1120515c27778219838c9303e
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-07-20 17:39:26 +0200

    auto: CommunityEventsPage.tsx

--- src/components/landing/CommunityEventsPage.tsx
+++ src/components/landing/CommunityEventsPage.tsx
@@ -160,7 +160,7 @@ export function CommunityEventsPage() {
 								<h1 className="leading-tighter text-3xl font-bold text-white">
 									Bring Effect to your community events
 								</h1>
-								<p className="mt-6 text-lg leading-relaxed text-zinc-400 md:max-w-[30rem]">
+								<p className="mt-6 text-lg leading-relaxed text-zinc-400 md:max-w-[66.67%] lg:max-w-[30rem]">
 									Speaking about Effect at a meetup, conference,{" "}
 									<br className="hidden md:block" />
 									or any developer event? We'll help you prepare, promote it,
