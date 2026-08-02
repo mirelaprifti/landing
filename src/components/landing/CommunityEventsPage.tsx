ea66ee04e48ef6a22993b2dda0df83a122074d43
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-08-02 19:13:16 +0200

    auto: CommunityEventsPage.tsx

--- src/components/landing/CommunityEventsPage.tsx
+++ src/components/landing/CommunityEventsPage.tsx
@@ -363,7 +363,7 @@ export function CommunityEventsPage() {
 											{item.description}
 										</p>
 										{/* Output */}
-										<ul className="mt-6 flex flex-col gap-3 border-t border-zinc-800 pt-6">
+										<ul className="mt-4 flex flex-col gap-3 border-t border-zinc-800 pt-4">
 											{item.details.map((detail) => (
 												<li
 													key={detail}
