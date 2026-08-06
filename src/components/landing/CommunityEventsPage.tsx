ba59091cc2571144175a93207a6c8ad570563bf6
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-08-06 17:25:39 +0200

    auto: CommunityEventsPage.tsx

--- src/components/landing/CommunityEventsPage.tsx
+++ src/components/landing/CommunityEventsPage.tsx
@@ -474,7 +474,7 @@ export function CommunityEventsPage() {
 				>
 					<div className="mx-auto w-full max-w-[73.75rem] px-4">
 						<p className="mb-3 font-mono text-sm font-medium tracking-wider text-zinc-400 uppercase">
-							// Talk ideas
+							// Effect Talk Ideas
 						</p>
 						<h2 className="leading-tighter text-2xl font-semibold text-white md:text-3xl">
 							Not sure what to talk about?
