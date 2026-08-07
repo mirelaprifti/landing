f76add628b62762ec5ab09f37265d5c95385154b
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-08-07 12:08:08 +0200

    auto: ApiReferencePage.tsx,DocsLayout.tsx Navigation.tsx

--- src/components/landing/ApiReferencePage.tsx
+++ src/components/landing/ApiReferencePage.tsx
@@ -87,7 +87,7 @@ export function ApiReferenceLayout({
 			>
 				Skip to main content
 			</a>
-			<Navigation activePath="/docs" wide compactSearch hideLinks />
+			<Navigation activePath="/docs" wide compactSearch />
 			<div className="relative w-full pt-16">
 				<DocsSectionTabs section="api" />
 				<div
