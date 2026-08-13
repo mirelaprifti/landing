b75ebf2380c33acda1e55a5c27a13b6613b91b62
Mirela Prifti <57798527+mirelaprifti@users.noreply.github.com>
2026-08-13 16:03:07 +0200

    auto: EffectDaysLivornoPage.tsx

--- src/components/landing/EffectDaysLivornoPage.tsx
+++ src/components/landing/EffectDaysLivornoPage.tsx
@@ -124,7 +124,7 @@ const SPONSORS = [
 	{
 		name: "Ziverge",
 		logo: "/assets/effect-days/ziverge.svg",
-		logoHeight: "h-10",
+		logoHeight: "h-9",
 		websiteUrl: "https://www.ziverge.com/",
 	},
 ];
